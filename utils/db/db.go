9a4c187a139768f5ca2bb6d6305d57fc295e5de1
Asuza <sora65536@outlook.com>
2024-09-15 18:05:33 +0800

    chore: 更新gorm日志接入

--- utils/db/db.go
+++ utils/db/db.go
@@ -31,72 +31,57 @@ type Config struct {
 	DbMap  map[string]DBInfo `yaml:"db"`
 }
 
-type zerologLogger struct {
+type Logger struct {
 	logger zerolog.Logger
 }
 
-func (zl *zerologLogger) LogMode(level logger.LogLevel) logger.Interface {
-	zl.logger.WithLevel(zerolog.Level(level))
-	return zl
+func (l Logger) LogMode(logger.LogLevel) logger.Interface {
+	return l
 }
 
-// Info 实现 GORM 的 Info 日志
-func (zl *zerologLogger) Info(ctx context.Context, msg string, data ...interface{}) {
-	if logger.LogLevel(zl.logger.GetLevel()) >= logger.Info {
-		zl.logger.Info().Msgf(msg, data...)
-	}
+func (l Logger) Error(ctx context.Context, msg string, opts ...interface{}) {
+	l.logger.Error().Msg(fmt.Sprintf(msg, opts...))
 }
 
-// Warn 实现 GORM 的 Warn 日志
-func (zl *zerologLogger) Warn(ctx context.Context, msg string, data ...interface{}) {
-	if logger.LogLevel(zl.logger.GetLevel()) >= logger.Warn {
-		zl.logger.Warn().Msgf(msg, data...)
-	}
+func (l Logger) Warn(ctx context.Context, msg string, opts ...interface{}) {
+	l.logger.Warn().Msg(fmt.Sprintf(msg, opts...))
 }
 
-// Error 实现 GORM 的 Error 日志
-func (zl *zerologLogger) Error(ctx context.Context, msg string, data ...interface{}) {
-	if logger.LogLevel(zl.logger.GetLevel()) >= logger.Error {
-		zl.logger.Error().Msgf(msg, data...)
-	}
+func (l Logger) Info(ctx context.Context, msg string, opts ...interface{}) {
+	l.logger.Info().Msg(fmt.Sprintf(msg, opts...))
 }
 
-// Trace 实现 GORM 的 Trace 日志（包括 SQL 语句、执行时间、错误等）
-func (zl *zerologLogger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
-	if logger.LogLevel(zl.logger.GetLevel()) <= 0 {
-		return
-	}
+func (l Logger) Trace(ctx context.Context, begin time.Time, fc func() (string, int64), err error) {
+	// if logger.LogLevel(zl.logger.GetLevel()) <= 0 {
+	// 	return
+	// }
 
 	elapsed := time.Since(begin) // 执行时间
 	sql, rows := fc()            // 获取 SQL 语句和影响的行数
 
 	switch {
 	case err != nil:
-		if logger.LogLevel(zl.logger.GetLevel()) >= logger.Error {
-			zl.logger.Error().
-				Err(err).
-				Str("sql", sql).
-				Int64("rows", rows).
-				Str("elapsed", fmt.Sprintf("%v", elapsed)).
-				Msg("SQL execution error")
-		}
+		l.logger.Error().
+			Err(err).
+			Str("sql", sql).
+			Int64("rows", rows).
+			Str("elapsed", fmt.Sprintf("%v", elapsed)).
+			Msg("SQL execution error")
 	case elapsed > time.Second: // 如果执行时间超过 1 秒，记录为 Warn 日志
-		if logger.LogLevel(zl.logger.GetLevel()) >= logger.Warn {
-			zl.logger.Warn().
-				Str("sql", sql).
-				Int64("rows", rows).
-				Str("elapsed", fmt.Sprintf("%v", elapsed)).
-				Msg("Slow SQL query")
-		}
+		l.logger.Warn().
+			Str("sql", sql).
+			Int64("rows", rows).
+			Str("elapsed", fmt.Sprintf("%v", elapsed)).
+			Msg("Slow SQL query")
+
 	default:
-		if logger.LogLevel(zl.logger.GetLevel()) >= logger.Info {
-			zl.logger.Info().
-				Str("sql", sql).
-				Int64("rows", rows).
-				Str("elapsed", fmt.Sprintf("%v", elapsed)).
-				Msg("SQL query executed")
-		}
+		l.logger.Info().
+			Str("sql", sql).
+			Int64("rows", rows).
+			Str("elapsed", fmt.Sprintf("%v", elapsed)).
+			Msg("SQL query executed")
 	}
+
 }
 
 func NewDbConfig() *Config {
@@ -114,14 +99,17 @@ func NewDbConfig() *Config {
 	if err != nil {
 		log.Error().Str("name", utilsName).Err(err).Msg("")
 	}
-	gormLogger := &zerologLogger{logger: log.Logger}
+	gormLogger := &Logger{logger: log.Logger}
 	gormLogger.LogMode(logger.Info)
 	for k, v := range config.DbMap {
 		//	ok2代表是否有该数据库，ok1代表是否开启该数据库，实际上不用的话，可以直接将数据库删除
 		if ok1, ok2 := config.DbType[v.Type]; ok2 && ok1 {
 			switch {
 			case v.Type == "pgsql":
-				v.Db, err = gorm.Open(postgres.Open(v.Dsn), &gorm.Config{Logger: gormLogger})
+				v.Db, err = gorm.Open(postgres.Open(v.Dsn), &gorm.Config{
+					Logger: gormLogger.LogMode(logger.Info),
+				})
+
 				if err != nil {
 					log.Error().Str("name", utilsName).Err(err).Msgf("Error open database %v", k)
 					continue
