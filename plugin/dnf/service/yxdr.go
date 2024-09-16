2f0fe091d06c5c062a707aa6ee1a16fac47c2602
Asuza <sora65536@outlook.com>
2024-09-16 12:22:31 +0800

    fix:增加yxdr超时时间

--- plugin/dnf/service/yxdr.go
+++ plugin/dnf/service/yxdr.go
@@ -58,7 +58,7 @@ func Screenshot(server string, productType string) ([]byte, string, error) {
 
 	// 导航到指定的URL
 	var buf []byte
-	ctx, _ := context.WithTimeout(ScCtx, 10*time.Second)
+	ctx, _ := context.WithTimeout(ScCtx, 20*time.Second)
 	// url := "https://www.baidu.com"
 	url := fmt.Sprintf("https://www.yxdr.com/bijiaqi/dnf/%s/kua%s", productType, ReportRegions[server])
 	err := chromedp.Run(ctx,
