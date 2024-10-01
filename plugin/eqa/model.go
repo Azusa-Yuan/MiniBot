06e14d141353d707ec9f526d6216b328a1cc7c65
Asuza <sora65536@outlook.com>
2024-10-01 08:48:27 +0800

    fix: eqa 空指针

--- plugin/eqa/model.go
+++ plugin/eqa/model.go
@@ -6,6 +6,6 @@ type eqa struct {
 	ID          int64           `gorm:"primaryKey"` // 自增主键
 	Key         string          `gorm:"column:key; index"`
 	Value       string          `gorm:"value"`
-	GID         int64           `gorm:"gid"`
+	GID         int64           `gorm:"column:gid"`
 	MessageList message.Message `gorm:"-"`
 }
