574c7125ea4de16a2741afae836513c9c1988999
Asuza <sora65536@outlook.com>
2024-10-20 19:32:19 +0800

    fix:修复中文情况下 lolicon接口请求出错

--- plugin/picture_package/picture.go
+++ plugin/picture_package/picture.go
@@ -6,13 +6,13 @@ import (
 	"io"
 	"math/rand/v2"
 	"net/http"
+	"net/url"
 	"regexp"
 	"strings"
 
 	zero "ZeroBot"
 	"ZeroBot/message"
 
-	"github.com/rs/zerolog/log"
 	"github.com/tidwall/gjson"
 )
 
@@ -87,18 +87,16 @@ func init() {
 func sendYmgal(y picturePackage, ctx *zero.Ctx, key, picType string) {
 	if y.PictureList == "" {
 		if picType != emoticonType {
-			resp, err := http.DefaultClient.Get("https://api.lolicon.app/setu/v2?tag=" + key)
-			log.Error().Err(err).Msg("")
+			encodedTag := url.QueryEscape(key)
+			resp, err := http.DefaultClient.Get("https://api.lolicon.app/setu/v2?tag=" + encodedTag)
 			if err == nil && resp.StatusCode == http.StatusOK {
 				respData, err := io.ReadAll(resp.Body)
-				log.Error().Err(err).Msg("")
 				if err == nil {
 					dataArray := gjson.ParseBytes(respData).Get("data").Array()
 					if len(dataArray) != 0 {
 						imgData := dataArray[0]
 						url := imgData.Get("urls").Get("original").String()
 						y.PictureList = url
-						log.Info().Msg(y.PictureList)
 						y.Title = imgData.Get("title").String() + "/" + imgData.Get("author").String()
 						y.Title = zero.BotConfig.NickName[0] + "暂时没有这样的图呢。" + "所以给你发这张" + y.Title
 					}
--- plugin/picture_package/picture_test.go
+++ plugin/picture_package/picture_test.go
@@ -2,8 +2,14 @@ package picturepackage
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"testing"
 	"time"
+
+	"github.com/rs/zerolog/log"
+	"github.com/tidwall/gjson"
 )
 
 func TestPage(t *testing.T) {
@@ -17,4 +23,23 @@ func TestPage(t *testing.T) {
 	}
 	fmt.Println(cgIDList)
 	fmt.Println(len(cgIDList))
+}
+
+func TestLolicon(t *testing.T) {
+	encodedTag := url.QueryEscape("刻晴")
+	resp, err := http.DefaultClient.Get("https://api.lolicon.app/setu/v2?tag=" + encodedTag)
+	log.Error().Err(err).Msg("")
+	if err == nil && resp.StatusCode == http.StatusOK {
+		respData, err := io.ReadAll(resp.Body)
+		log.Error().Err(err).Msg("")
+		if err == nil {
+			fmt.Println(gjson.ParseBytes(respData).Get("data"))
+			dataArray := gjson.ParseBytes(respData).Get("data").Array()
+			if len(dataArray) != 0 {
+				imgData := dataArray[0]
+				url := imgData.Get("urls").Get("original").String()
+				fmt.Println(url)
+			}
+		}
+	}
 }
