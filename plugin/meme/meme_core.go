90ffb22336fc3ba71d16f6001d97c7ea40b0293a
AsuzaYuan <sora65536@outlook.com>
2025-01-01 11:00:15 +0000

    fix:调整meme帮助图片字体大小

--- plugin/meme/meme_core.go
+++ plugin/meme/meme_core.go
@@ -51,14 +51,14 @@ func GetHelp() ([]byte, error) {
 	/***********设置字体颜色为黑色***********/
 	canvas.SetRGB(0, 0, 0)
 	/***********设置字体大小,并获取字体高度用来定位***********/
-	if err = canvas.ParseFontFace(data, fontSize*2); err != nil {
+	if err = canvas.ParseFontFace(data, fontSize*1.5); err != nil {
 		return nil, err
 	}
 	sl, h := canvas.MeasureString("表情包列表")
 	/***********绘制标题***********/
 	canvas.DrawString("表情包列表", (width-sl)/2, 140-1.2*h) // 放置在中间位置
 	/***********设置字体大小,并获取字体高度用来定位***********/
-	if err = canvas.ParseFontFace(data, 1.5*fontSize); err != nil {
+	if err = canvas.ParseFontFace(data, 1.2*fontSize); err != nil {
 		return nil, err
 	}
 
